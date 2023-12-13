0ab5e6557451239bbd9332acaebfdb3bf177c2e1
amatsagu <krzysztof.rapior@protonmail.com>
2023-12-13 20:55:54 +0100

    feat: export shared logic

---
+++ control/gsettings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface cursor-theme "breeze_cursors"
+gsettings set org.gnome.desktop.interface enable-animations true
+gsettings set org.nemo.desktop font "Noto Sans 10"
+gsettings set org.gnome.desktop.interface font-name "Noto Sans 10"
+gsettings set org.gnome.desktop.interface document-font-name "Noto Sans 10"
+gsettings set org.cinnamon.desktop.wm.preferences titlebar-font "Sans Bold 10"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Sans Bold 10"
+gsettings set org.gnome.desktop.interface monospace-font-name "Monospace 11"
+gsettings set org.gnome.desktop.interface font-antialiasing "rgba"
+gsettings set org.gnome.desktop.interface font-hinting "slight"
+gsettings set org.gnome.desktop.interface font-rgba-order "rgb"
+gsettings set org.gnome.desktop.interface gtk-color-palette "black:white:gray50:red:purple:blue:light blue:green:yellow:orange:lavender:brown:goldenrod4:dodger blue:pink:light green:gray10:gray30:gray75:gray90"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+gsettings set org.gtk.Settings.ColorChooser custom-colors "[(0.81568627450980391, 0.81176470588235294, 0.80000000000000004, 1.0), (0.20000000000000001, 0.7803921568627451, 0.87058823529411766, 1.0), (0.16470588235294117, 0.63137254901960782, 0.70196078431372544, 1.0), (0.63921568627450975, 0.27843137254901962, 0.72941176470588232, 1.0), (0.16470588235294117, 0.4823529411764706, 0.87058823529411766, 1.0), (0.070588235294117646, 0.28235294117647058, 0.54509803921568623, 1.0), (0.9882352941176471, 0.9137254901960784, 0.30980392156862746, 1.0), (0.9137254901960784, 0.67843137254901964, 0.047058823529411764, 1.0)]"
+gsettings set org.gtk.Settings.ColorChooser selected-color (true, 1.0, 1.0, 1.0, 1.0)
