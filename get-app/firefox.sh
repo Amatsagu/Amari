f9c69309959e94b6270bec41efe9a5ae83ae23cf
amatsagu <krzysztof.rapior@protonmail.com>
2023-12-13 21:45:08 +0100

    feat: register installed browser as default choice

--- get-app/firefox.sh
+++ get-app/firefox.sh
@@ -11,6 +11,7 @@ sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /usr/share/
 sudo ln -s /usr/share/firefox/firefox /usr/bin/firefox
 rm ~/FirefoxSetup.tar.bz2
 sudo cp ./desktop/firefox.desktop /usr/share/applications/firefox.desktop 
+xdg-settings set default-web-browser firefox.desktop
 
 printf "\n\n\n\n\n\n\n\n\n\n"
 
--- get-app/google-chrome.sh
+++ get-app/google-chrome.sh
@@ -10,6 +10,7 @@ wget -O ~/GoogleChromeSetup.deb "https://dl.google.com/linux/direct/google-chrom
 sudo apt install ~/GoogleChromeSetup.deb -y
 rm ~/GoogleChromeSetup.deb
 sudo cp ./desktop/google-chrome.desktop /usr/share/applications/google-chrome.desktop
+xdg-settings set default-web-browser google-chrome.desktop
 
 printf "\n\n\n\n\n\n\n\n\n\n"
 
