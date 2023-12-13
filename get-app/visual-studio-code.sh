f8d582b993497307ce7eef9c2c080b6df1edd5a9
amatsagu <krzysztof.rapior@protonmail.com>
2023-12-13 21:40:49 +0100

    feat: add microsoft gpg key to trusted apt sources for auto update

--- get-app/visual-studio-code.sh
+++ get-app/visual-studio-code.sh
@@ -13,20 +13,16 @@ sudo cp ./desktop/code.desktop /usr/share/applications/code.desktop
 sudo rm /usr/share/applications/code-url-handler.desktop
 xdg-mime default code.desktop text/plain
 
+curl -sS https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/microsoft.gpg
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
 printf "\n\n\n\n\n\n\n\n\n\n"
 
 cat << EOF
 # ===========================================================================
 # FINISHED INSTALLATION
-#
-# Warning
-# This script uses latest version of stable VSC, outside Debian's
-# apt stream - you'll have to run this script again for major updates.
-#
-# Note
-# Visual studio code does not require being constantly updated. You can use
-# older versions of VSC just fine however newer versions may
-# improve Wayland compability.
-#
 # ===========================================================================
 EOF
