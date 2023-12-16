c2f8c0cec6c0d61db1511eeff908fc317201016c
amatsagu <krzysztof.rapior@protonmail.com>
2023-12-16 17:38:21 +0100

    fix: add auto install flag

--- get-app/visual-studio-code.sh
+++ get-app/visual-studio-code.sh
@@ -14,9 +14,9 @@ EOF
 
 curl -sS https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt install apt-transport-https
+sudo apt install apt-transport-https -y
 sudo apt update
-sudo apt install code
+sudo apt install code -y
 sudo cp ./desktop/code.desktop /usr/share/applications/code.desktop
 sudo rm /usr/share/applications/code-url-handler.desktop
 xdg-mime default code.desktop text/plain
